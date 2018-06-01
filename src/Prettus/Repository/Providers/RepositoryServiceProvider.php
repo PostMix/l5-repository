d372c7de6d034f951b9447bbb29926f3053515a8
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2018-06-01 23:57:45 +0300

    Added tests to generation of files

--- src/Prettus/Repository/Providers/RepositoryServiceProvider.php
+++ src/Prettus/Repository/Providers/RepositoryServiceProvider.php
@@ -50,6 +50,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->commands('Prettus\Repository\Generators\Commands\ControllerCommand');
         $this->commands('Prettus\Repository\Generators\Commands\BindingsCommand');
         $this->commands('Prettus\Repository\Generators\Commands\CriteriaCommand');
+        $this->commands('Prettus\Repository\Generators\Commands\ProjectCommand');
         $this->app->register('Prettus\Repository\Providers\EventServiceProvider');
     }
 
